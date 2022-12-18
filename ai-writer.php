afeb2416750c370013542c6fa653345bece8cfb6
Dipl. Ing. (FH) Florian Simeth <florian@florian-simeth.de>
2022-12-18 21:12:29 +0100

    Updated version number to 0.2.0

--- ai-writer.php
+++ ai-writer.php
@@ -4,7 +4,7 @@
  * Description:       When the AI completes your sentence... Register at https://aiwriter.space to use this plugin! Thanks.
  * Requires at least: 6.1.0
  * Requires PHP:      8.0.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            floriansimeth
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
