760d918794b754dd907f5489854dc775d2755c35
Dipl. Ing. (FH) Florian Simeth <florian@florian-simeth.de>
2023-01-27 05:27:49 +0100

    Remove plugin data from WP database table after uninstall

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+if ( ! WP_UNINSTALL_PLUGIN ) {
+	exit();
+}
+
+delete_option( 'aiwriter/activation_code' );
+
+global $wpdb;
+
+$wpdb->delete( $wpdb->usermeta, [ 'meta_key' => 'aiwriter_isActive' ] );
+$wpdb->delete( $wpdb->usermeta, [ 'meta_key' => 'aiwriter_temperature' ] );
+$wpdb->delete( $wpdb->usermeta, [ 'meta_key' => 'aiwriter_textLength' ] );
