06122df1b71812c980cf4b44ceb83f9b5826af1b
Dipl. Ing. (FH) Florian Simeth <florian@florian-simeth.de>
2023-02-10 14:27:21 +0100

    New dependency: pasteHandler to better include response from the AI

--- src/block-editor.js
+++ src/block-editor.js
@@ -6,6 +6,7 @@ import AiWriterSidebar from "./sidebar";
 import {subscribe, select, dispatch} from '@wordpress/data';
 import {__, sprintf} from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
+import {pasteHandler} from '@wordpress/blocks';
 // end dependencies
 
 const AiWriter = window.AiWriter;
