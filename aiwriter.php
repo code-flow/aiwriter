96755d6a22b0c7bf9efc6affd9a61dcfcace3f54
Dipl. Ing. (FH) Florian Simeth <florian@florian-simeth.de>
2023-02-28 19:43:55 +0100

    Fixed issue where multiple messages were displayed due to the Classic Editor script being loaded even though the Block Editor was being used.

--- aiwriter.php
+++ aiwriter.php
@@ -541,11 +541,6 @@ function enqueueBlockEditorScripts(): void {
 		return;
 	}
 
-	if ( ! isset( $screen->is_block_editor ) ) {
-		return;
-	}
-
-
 	if ( ! function_exists( 'get_plugin_data' ) ) {
 		$file = ABSPATH . 'wp-admin/includes/plugin.php';
 
@@ -818,6 +813,20 @@ add_action( 'after_wp_tiny_mce', 'wpbuddy\ai_writer\classicEditorTinyMceScripts'
  * @since 0.4.0
  */
 function classicEditorTinyMceScripts(): void {
+	$screen = get_current_screen();
+
+	if ( ! $screen instanceof \WP_Screen ) {
+		return;
+	}
+
+	if ( ! isset( $screen->is_block_editor ) ) {
+		return;
+	}
+
+	if ( $screen->is_block_editor ) {
+		return;
+	}
+
 	?>
 	<script>
 		tinymce.on('addeditor', function (event) {
